e5f3f4cec7a842bd9e11537f5dab4d0bfc6e20e3
JKrayger <jk1019602@gmail.com>
2023-08-17 14:00:36 -0400

    dvd screensaver

--- phys.py
+++ phys.py
@@ -1,19 +1,15 @@
 from __future__ import division
 from vpython.no_notebook import stop_server
 from vpython import *
-import time
-import math
-import numpy as np
 
-earthMass = 5.97e24
-cubeX = 0.0
+cubeX = 0.5
 cubeY = 1.0
-cubeZ = 0.0
+cubeZ = 0
 
 cube = box(color = color.green,
            size = vector(0.5, 0.5, 0.5),
            mass = 1.0,
-           pos = vector(0, 3.0, 0))
+           pos = vector(0.5, 3.0, 0))
 
 planeTop = box(color = color.cyan,
             size = vector(2, 0.2, 2),
@@ -24,14 +20,14 @@ planeBtm = box(color = color.cyan,
             pos = vector(0, 0, 0))
 
 planeLeft = box(color = color.cyan,
-            size = vector(2, 0.2, 2),
-            pos = vector(-1.0, 1.5, 0.0))
+            size = vector(3.2, 0.2, 2),
+            pos = vector(-1.1, 1.5, 0.0))
 planeLeft.rotate(angle = 2 * pi / 4,
                  axis = vector(0, 0, 1))
 
 planeRight = box(color = color.cyan,
-            size = vector(2, 0.2, 2),
-            pos = vector(1.0, 1.5, 0))
+            size = vector(3.2, 0.2, 2),
+            pos = vector(1.1, 1.5, 0))
 planeRight.rotate(angle = 2 * pi / 4,
                  axis = vector(0, 0, 1))
 
@@ -42,21 +38,41 @@ def collisionY(c, pt, pb):
 
     if(round(c.pos.y, 2) == pbPos or round(c.pos.y, 2) == ptPos):
         return True
+    
+def collisionX(c, pl, pr):
+    plPos = round(pl.pos.x + ((pl.size.x / 2) + (c.size.x / 2)), 2)
+    prPos = round(pr.pos.x - ((pr.size.x / 2) + (c.size.x / 2)), 2)
+
+    if(round(c.pos.x, 2) == plPos or round(c.pos.x, 2) == prPos):
+        return True
 
 
 go = True
-sign = 1
+switch = True
+ySign = 1
+xSign = 1
 
 while(go):
     rate(928)
-
     if(collisionY(cube, planeTop, planeBtm)):
-        sign = sign * -1
-        
-    cube.rotate(angle = sign * (2 * pi) / 32,
+        ySign = ySign * -1
+        switch = True
+    elif(collisionX(cube, planeLeft, planeRight)):
+        xSign = xSign * -1
+        switch = False
+
+    
+    cube.rotate(angle = (2 * pi) / 32,
                 axis = vector(1, 1, 0))
     
-    cubeY = cubeY + (sign * 0.001)
+
+    if(switch):
+        cubeY = cubeY + (ySign * 0.001)
+        cubeX = cubeX + (xSign * 0.001)
+    else:
+        cubeY = cubeY + (ySign * 0.001)
+        cubeX = cubeX + (xSign * 0.001)
+
     cube.pos = vector(cubeX, cubeY, cubeZ)
     
 
